743f375b81fff1bbee71aba8678af6648dc7cb61
Tyler Simko <tsimko@princeton.edu>
2014-04-07 22:56:19 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sadie::Application.config.secret_key_base = '80f79f642b0d6146f07a50338f54c21549dde74d5e88f26c2cda9fa70a4a1cdebd2d7a8d1a6db5590e76d3f90e2f1131d083680e122dc6d7fb506351e2ed9afc'
